c7801ab33442fd6387042ded9c6ae29c09aae8fa
Jonathan <jony.antolini@gmail.com>
2020-05-22 17:23:03 -0300

    con wa funcionando??

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -5,6 +5,8 @@ import Hero from 'react-bulma-components/lib/components/hero';
 import Button from 'react-bulma-components/lib/components/button';
 import Section from 'react-bulma-components/lib/components/section';
 import Container from 'react-bulma-components/lib/components/container';
+import Box from 'react-bulma-components/lib/components/box';
+import List from 'react-bulma-components/lib/components/list';
 
 import { FaBeer } from 'react-icons/fa';
 import { FaPlus } from 'react-icons/fa';
@@ -47,12 +49,27 @@ export const CartDrawer = () => {
     const [cart, setCart] = useContext(CartContext);
     //const totalPrice = cart.reduce((acc, curr) => acc + curr.price, 0);
   
+    const rows = cart.map(( r, i ) => {
+        return (
+            <List.Item key={i}>Cerveza: {r.name}</List.Item>
+        ) 
+      })
+
     return (
+    <Box>
     <div>
-        <strong>TU PEDIDO ( {cart.length}) <br/> </strong>
+      <Section>
+        <strong>Listado de tu Pedido ( {cart.length}) <br/> </strong>
+        <Box>
+          <List hoverable>
+            {rows}
+          </List>
+        </Box>
         <span>
-            <a target="_blank" href="https://api.whatsapp.com/send?phone=2984901230Hola%2c%20quería%20pedir%3a%0a%0a%2a%20%5bEnsaladas%5d%20-%20Ensalada%20italiana%20pequeña%20picada%20-%20Elegí%20la%20carne%3a%20Panceta%20-%20%24350%0a%0aTotal%3a%20%24350%0a%0aGracias.&source=&data=&app_absent=">PEDIR POR WHATSAPP</a>
+            <a target="_blank" href="https://api.whatsapp.com/send?phone=5492665030860&text=Hola%2c%20quer%c3%ada%20pedir estas cervezas%3a%0a%0a%2a%20%5bTotal%3a%20%24250%0a%0aGracias.&source=&data=&app_absent=">PEDIR POR WHATSAPP</a>
         </span>
+        </Section>
     </div>
+    </Box>
     )
 }
