06b491d4f8e1214fdd725b5aca0e0adcd13b2ec6
Jonathan <jony.antolini@gmail.com>
2020-06-19 08:13:38 -0300

    prueba

--- src/components/Foo.js
+++ src/components/Foo.js
@@ -19,17 +19,74 @@ export default function ProductItem(props) {
         })
     }
 
+    const style = { textAlign: 'center' }
     return (
-        <div>
-            <div>Contador de compras: {state.quantityTotal}</div>
-            <Image src="birra.jpg" />
-            <Button
-                className="button is-rounded is-small"
-                renderAs="span"
-                onClick={foo}
-            >
-                +
-            </Button>
-        </div>
+
+        <Tile kind="ancestor">
+            <Tile size={12}>
+                <Tile>
+                    <Tile kind="parent">
+                        <Tile className="box" size={4} renderAs="article" kind="child" key={1}>
+                        <Columns style={style}>
+                            <div>Contador de compras: {state.quantityTotal}</div>
+                            <Columns.Column>
+                                <Heading>American IPA</Heading>
+                                <Heading subtitle>Suave</Heading>
+                            </Columns.Column>
+                        </Columns>
+                        <Image src="birra.jpg" />
+                        <Columns style={style}>
+                            <Columns.Column>
+                                <p>$100 x unidad</p>
+                            </Columns.Column>
+                            <Columns.Column>
+                                <div className="actions">
+                                    <Button
+                                        className="button is-rounded is-small"
+                                        renderAs="span"
+                                        onClick={foo}
+                                    >
+                                        +
+                                    </Button>
+                                </div>
+                            </Columns.Column>
+                            <Columns.Column>
+                                <p className="bd-notification is-success"><small>*unidad referencia <br/> botella 500ml</small></p>
+                            </Columns.Column>
+                        </Columns>
+                    </Tile>
+                    <Tile className="box" size={4} renderAs="article" kind="child" key={2}>
+                        <Columns style={style}>
+                            <div>Contador de compras: {state.quantityTotal}</div>
+                            <Columns.Column>
+                                <Heading>American IPA</Heading>
+                                <Heading subtitle>Suave</Heading>
+                            </Columns.Column>
+                        </Columns>
+                        <Image src="birra.jpg" />
+                        <Columns style={style}>
+                            <Columns.Column>
+                                <p>$100 x unidad</p>
+                            </Columns.Column>
+                            <Columns.Column>
+                                <div className="actions">
+                                    <Button
+                                        className="button is-rounded is-small"
+                                        renderAs="span"
+                                        onClick={foo}
+                                    >
+                                        +
+                                    </Button>
+                                </div>
+                            </Columns.Column>
+                            <Columns.Column>
+                                <p className="bd-notification is-success"><small>*unidad referencia <br/> botella 500ml</small></p>
+                            </Columns.Column>
+                        </Columns>
+                    </Tile>
+                    </Tile>
+                </Tile>
+            </Tile>
+        </Tile>
     )
 }
